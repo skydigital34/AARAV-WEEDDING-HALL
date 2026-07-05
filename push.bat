54c219b53ba7dd151ed585633055a2ace45ec19b
Skill Star <skillstar66@gmail.com>
2026-07-05 22:38:41 +0530

    Push to wedding hall repository

--- push.bat
+++ push.bat
@@ -2,6 +2,6 @@
 cd /d "d:\SK-TECH SOLUTIONS\sites\wedding hall"
 git add .
 git commit -m "Push to wedding hall repository"
-git remote set-url origin https://github.com/Ramkumar143z/WEDDINGHALL.git 2>nul || git remote add origin https://github.com/Ramkumar143z/WEDDINGHALL.git
+git remote set-url origin https://github.com/skydigital34/AARAV-WEEDDING-HALL.git 2>nul || git remote add origin https://github.com/skydigital34/AARAV-WEEDDING-HALL.git
 git branch -M main
 git push -u origin main
